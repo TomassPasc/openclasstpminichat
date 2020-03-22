ec9944d3cd8b2128e25d364c0f9bfcaa9b1f8b20
ThomasPascual <thoma1@free.fr>
2020-03-22 17:55:16 +0100

    minichat.php: créer le lien avec la bdd

--- minichat.php
+++ minichat.php
@@ -7,6 +7,8 @@
 
     <body>
 
+            <!--Formulaire -->
+
     <form action="minichat_post.php" method="post">
         <p>
             <label>Pseudo :<input type="text" name="pseudo" /></label><br />
@@ -14,9 +16,26 @@
             <input type="submit" value="Envoyer" />
         </p>
     </form>
-       
+         <!--le mini chat -->
+<?php
+    try
+    {
+        $bdd = new PDO('mysql:host=localhost;dbname=test;charset=utf8', 'root', '',
+        array(PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION)); 
+    }
+    catch(Exception $e)
+    {
+            die('Erreur : '.$e->getMessage());
+    }
+    $reponse = $bdd->query('SELECT * FROM manichat ORDER BY ID DESC LIMIT 0,10');
 
-    
-    
+
+    while ($donnees = $reponse->fetch()){
+        echo 'good';
+
+    }
+
+
+ ?>   
     </body>
 </html>
--- minichat_post.php
+++ minichat_post.php
@@ -4,7 +4,7 @@
 try
 {
     $bdd = new PDO('mysql:host=localhost;dbname=test;charset=utf8', 'root', '',
-    array(PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION)); //permet d'afficher les erreurs précises
+    array(PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION)); 
 }
 catch(Exception $e)
 {
