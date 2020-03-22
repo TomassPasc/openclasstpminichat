a73bf323f0bd94d5db6797c15333efc3885fc128
ThomasPascual <thoma1@free.fr>
2020-03-22 17:30:56 +0100

    ajout du header pour rediriger l'utilisateur sur la page principal + ajout commentaires

--- minichat_post.php
+++ minichat_post.php
@@ -1,4 +1,6 @@
 <?php
+
+//connexion à la BDD
 try
 {
     $bdd = new PDO('mysql:host=localhost;dbname=test;charset=utf8', 'root', '',
@@ -9,8 +11,13 @@ catch(Exception $e)
         die('Erreur : '.$e->getMessage());
 }
 
+// Insertion des données envoyée dans la BDD
+
 $req = $bdd->prepare('INSERT INTO manichat(pseudo, message) VALUES (:pseudo, :message)');
 $req ->execute(array(
     'pseudo' => $_POST['pseudo'],
     'message' => $_POST['message']
 ));
+
+// L'utilisateur est redirigé directement sur la page principal
+header('Location: minichat.php'); 
