5216a4997561e678be50e16a5ec89fadc7250bae
ThomasPascual <thoma1@free.fr>
2020-03-22 17:01:46 +0100

    formulaire réalisé dans minichat.php

---
+++ minichat.php
@@ -0,0 +1,22 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <meta charset="utf-8" />
+        <title>Mini chat</title>
+    </head>
+
+    <body>
+
+    <form action="minichat_post.php" method="post">
+        <p>
+            <label>Pseudo :<input type="text" name="pseudo" /></label><br />
+            <label>Message: <input type="text" name="message" /></label><br />
+            <input type="submit" value="Envoyer" />
+        </p>
+    </form>
+       
+
+    
+    
+    </body>
+</html>
